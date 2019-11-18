42888509e8b28b10331ee39fde1dba2adf4e7790
simoncdesmet <simoncdesmet@gmail.com>
2019-11-18 11:00:36 +0100

    Initial commit

---
+++ src/test/java/com/dreamteam/parkshark/parkshark/ParksharkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamteam.parkshark.parkshark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParksharkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
