aefb82a5d8717b126502e12054341d056bcaffa8
simoncdesmet <simoncdesmet@gmail.com>
2019-11-19 09:47:18 +0100

    fixed application startup problem

--- api/src/main/java/com/dreamteam/parkshark/api/ParkSharkApplication.java
+++ api/src/main/java/com/dreamteam/parkshark/api/ParkSharkApplication.java
@@ -2,8 +2,12 @@ package com.dreamteam.parkshark.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.dreamteam.parkshark")
+@EnableJpaRepositories(basePackages = "com.dreamteam.parkshark.repository")
+@EntityScan(basePackages = "com.dreamteam.parkshark.domain")
 public class ParkSharkApplication {
 
     public static void main(String[] args) {
