2514f6050469005fe730c67f3a0106ede9c38e3b
hansdewitte <hgjdewitte@gmail.com>
2019-11-20 13:23:34 +0100

    fixed the test. Story ME1 reviewed.

--- domain/src/main/java/com/dreamteam/parkshark/domain/Address.java
+++ domain/src/main/java/com/dreamteam/parkshark/domain/Address.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name = "ADDRESS")
 public class Address {
     @Id
     @SequenceGenerator(name = "ADDRESS_SEQ", sequenceName = "ADDRESS_SEQ", allocationSize = 1)
